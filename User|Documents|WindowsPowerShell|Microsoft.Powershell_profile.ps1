c3b8adc579e4cc2102caab80b154f86240e59d48
Paul Masek <30813009+itpropaul@users.noreply.github.com>
2018-03-28 08:26:58 -0400

    removed older barely used functions

--- User|Documents|WindowsPowerShell|Microsoft.Powershell_profile.ps1
+++ User|Documents|WindowsPowerShell|Microsoft.Powershell_profile.ps1
@@ -2,15 +2,6 @@
 ## Module Path = C:\Program Files\WindowsPowerShell\Modules ##
 ##############################################################
 
-# A couple functions for frequently used git commands that don't require parameters
-function gitp {git push}
-function gits {git status}
-
-# Use Bash Vim from powershell
-# Make sure to use this formatting: vi /mnt/c/Users/pmasek/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1 
-function vi ($File){
-bash -c "vi $File"
-}
 
 # Looks up the command name for an alias like Get-Alias, but also returns other aliases associated with the discovered command.
 function Get-AllAliases ($Name){
@@ -36,63 +27,6 @@ Function Get-FileSize ($File){
 ls $File | Select Name, @{Name="Size";Expression={Format-FileSize($_.Length)}}
 }
 
-# The previous code here to make commandline history persistent is no longer needed. See these two articles for how / where Powershell
-# saves the commandline history and how to access it.
-# https://blogs.technet.microsoft.com/heyscriptingguy/2014/06/18/better-powershell-history-management-with-psreadline/
-# https://www.jaapbrasser.com/quicktip-powershell-command-history-on-windows-10-using-psreadline/
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-# The following two commands are based on https://github.com/alexanderepstein/Bash-Snippets that I
-# have installed on Bash on Windows (via the "Git Install" method - will be able to update via "Git Update").
-
-# Show the weather for 46807 upon PowerShell load
-bash -c "weather 46807"
-
-# Show the available Bash-Snippets Utilities
-bash -c "echo 'Bash-Snippets Utilities (To run from PowerShell: bash -c comand):';cat /mnt/c/Users/pmasek/Bash-Snippets/README.md |grep -m 19 '^<summary>' |cut -f2 -d'>' |cut -f1 -d'<' |tr -d '\n';echo"
-
-# from: http://community.idera.com/powershell/powertips/b/tips/posts/find-installed-software
-function Get-InstalledSoftware
-{
-    param
-    (
-        $DisplayName='*',
-
-        $DisplayVersion='*',
-
-        $UninstallString='*',
-
-        $InstallDate='*'
-
-    )
-    
-    # registry locations where installed software is logged
-    $pathAllUser = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    $pathCurrentUser = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    $pathAllUser32 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    $pathCurrentUser32 = "Registry::HKEY_CURRENT_USER\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-
-   
-    # get all values
-    Get-ItemProperty -Path $pathAllUser, $pathCurrentUser, $pathAllUser32, $pathCurrentUser32 |
-      # choose the values to use
-      Select-Object -Property DisplayVersion, DisplayName, UninstallString, InstallDate |
-      # skip all values w/o displayname
-      Where-Object DisplayName -ne $null |
-      # apply user filters submitted via parameter:
-      Where-Object DisplayName -like $DisplayName |
-      Where-Object DisplayVersion -like $DisplayVersion |
-      Where-Object UninstallString -like $UninstallString |
-      Where-Object InstallDate -like $InstallDate |
-
-      # sort by displayname
-      Sort-Object -Property DisplayName 
-}
 
 # from: http://community.idera.com/powershell/powertips/b/tips/posts/pipe-information-to-excel
 # This function pipes to Excel and instantly opens in Excel. It puts the csv in temp, so that you don't need to clean up after.
@@ -106,4 +40,16 @@ function Out-Excel
   $Input | 
     Export-Csv $path -NoTypeInformation -UseCulture -Encoding UTF8
     Invoke-Item $path 
+}
+
+# The previous code here to make commandline history persistent is no longer needed. See these two articles for how / where Powershell
+# saves the commandline history and how to access it.
+# https://blogs.technet.microsoft.com/heyscriptingguy/2014/06/18/better-powershell-history-management-with-psreadline/
+# https://www.jaapbrasser.com/quicktip-powershell-command-history-on-windows-10-using-psreadline/
+# Chocolatey profile
+
+# Installed by Chocolatey install script
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
